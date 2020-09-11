bf157dbf57b433c83defeb2584c6022f3076ed2d
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-11 11:20:01 +0800

    update

---
+++ 算法/中级算法/数学/Excel表列序号.cpp
@@ -0,0 +1,14 @@
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int titleToNumber(string s) 
+    {
+        int res=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            res+=(s[i]-'A'+1)*pow(26,s.length()-i-1);
+        }
+        return res;
+    }
+};
