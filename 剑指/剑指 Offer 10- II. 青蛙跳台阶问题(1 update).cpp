e0a7e26049ed1f7924e1d8faf4ca173777e91fdd
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-22 20:07:01 +0800

    update 70

--- 剑指/剑指 Offer 10- II. 青蛙跳台阶问题(1 update).cpp
+++ 剑指/剑指 Offer 10- II. 青蛙跳台阶问题(1 update).cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int numWays(int n) {
-        if(n==0)
-            return 1;
-        if(n==1)
-            return 1;
-        else if(n==2)
-            return 2;
-        else 
-            return (numWays(n-1)+numWays(n-2))%((int)1e9+7);
+        long dp[n+10];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=(dp[i-1]+dp[i-2])%((int)1e9+7);
+        }
+        return dp[n];
     }
 };
