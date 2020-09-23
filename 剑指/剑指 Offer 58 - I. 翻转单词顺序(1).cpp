defa101bbc71fa523d76211f2cc28bb7186a160c
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-23 20:51:48 +0800

    update 剑指 官方答案 58

---
+++ 剑指/剑指 Offer 58 - I. 翻转单词顺序(1).cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include<iostream>
+#include<sstream>
+using namespace  std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss;
+        string ans="",temp;
+        ss<<s;
+        while(ss>>temp){
+            ans=" "+temp+ans;
+        }
+        if(ans!="")
+            ans.erase(ans.begin());
+        return ans; 
+    }
+};
