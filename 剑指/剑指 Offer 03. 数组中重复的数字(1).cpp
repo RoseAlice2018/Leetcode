884c188fbd2ce67bfb224c9b3cf002e8cff64f01
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-25 20:04:12 +0800

    update

---
+++ 剑指/剑指 Offer 03. 数组中重复的数字(1).cpp
@@ -0,0 +1,23 @@
+
+#include<vector>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        int temp;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(nums[i]!=i){
+                if(nums[i]==nums[nums[i]])
+                {
+                    return nums[i];
+                }
+                temp=nums[i];
+                nums[i]=nums[temp];
+                nums[temp]=temp;
+            }
+        }
+        return 0;
+    }
+};
