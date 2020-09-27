062ce384528b537e98dee7e198e6bd3bf9cb44f2
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-27 22:40:42 +0800

    update

---
+++ 每日一题/Sep/9.27.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        if(root->val==p->val)
+            return p;
+        if(root->val==q->val)
+        {
+            return q;
+        }
+        if((p->val<root->val&&q->val>root->val)||(p->val>root->val&&q->val<root->val))
+        {
+            return root;
+        }
+        if(q->val<root->val&&p->val<root->val)
+        {
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        else if(q->val>root->val&&p->val>root->val)
+        {
+            return lowestCommonAncestor(root->right,p,q);
+        } 
+        
+        return root;
+        
+    }
+};
