00bb1fec14eb355f06b25017524f5dfa74ccf0ad
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-09-30 11:24:49 +0800

    update

---
+++ 剑指/剑指 Offer 17. 打印从1到最大的n位数.cpp
@@ -0,0 +1,11 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> printNumbers(int n) {
+        vector<int> res;
+        for(int i=1;i<pow(10,n);i++)
+            res.push_back(i);
+        return res;
+    }
+};
