e477b4851f8d61db7a8340ac74e61cb3e3c1e0c9
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-06 22:16:38 +0800

    update 剑指 61

--- 剑指/剑指 Offer 61. 扑克牌中的顺子.cpp
+++ 剑指/剑指 Offer 61. 扑克牌中的顺子.cpp
@@ -1,38 +1,50 @@
 #include<vector>
+#include<set>
 using namespace std;
 class Solution {
 public:
     bool isStraight(vector<int>& nums) {
         int max=INT_MIN;
         int min=INT_MAX;
-        int tag=0;
+       // int tag=0;
+        set<int> num;
         for(int i=0;i<nums.size();i++)
+           {
+               if(num.count(nums[i])&&nums[i]!=0)
+                    return false;
+                num.insert(nums[i]);
+           }
+        set<int>::iterator it=num.begin();
+        
+        for(;it!=num.end();it++)
         {
-            if(nums[i]==0)
+            if(*it==0)
                 ;//skip;
             else
             {
-                if(nums[i]==1)
-                {
-                    //special tackle
-                    tag=1;
-                }
-                else
-                {
-                    if(nums[i]>max)
-                        max=nums[i];
-                    if(nums[i]<min)
-                        min=nums[i];
-                }
+                // if(*it==1)
+                // {
+                //     //special tackle
+                //     tag=1;
+                // }
+                // else
+               // {
+                    if(*it>max)
+                        max=*it;
+                    if(*it<min)
+                        min=*it;
+              //  }
             }  
         }
-        if(tag)
-        {
-            if(min>=10)
-                max=14;
-            if(max<=5)
-                min=1;
-        }
+        // if(tag)
+        // {
+        //     if(min>=10)
+        //         max=14;
+        //     else if(max<=5)
+        //         min=1;
+        //     else 
+        //         return false;
+        // }
        
         if(max-min<5)
             return true;
