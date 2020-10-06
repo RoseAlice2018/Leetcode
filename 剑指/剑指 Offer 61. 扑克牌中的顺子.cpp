ad9417abba43fa3e8d7971835b416399e498e57e
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-06 22:02:57 +0800

    update 10.6

--- 剑指/剑指 Offer 61. 扑克牌中的顺子.cpp
+++ 剑指/剑指 Offer 61. 扑克牌中的顺子.cpp
@@ -3,6 +3,40 @@ using namespace std;
 class Solution {
 public:
     bool isStraight(vector<int>& nums) {
+        int max=INT_MIN;
+        int min=INT_MAX;
+        int tag=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+                ;//skip;
+            else
+            {
+                if(nums[i]==1)
+                {
+                    //special tackle
+                    tag=1;
+                }
+                else
+                {
+                    if(nums[i]>max)
+                        max=nums[i];
+                    if(nums[i]<min)
+                        min=nums[i];
+                }
+            }  
+        }
+        if(tag)
+        {
+            if(min>=10)
+                max=14;
+            if(max<=5)
+                min=1;
+        }
+       
+        if(max-min<5)
+            return true;
         
+        return false;
     }
 };
