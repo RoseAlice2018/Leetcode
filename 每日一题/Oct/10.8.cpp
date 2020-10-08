caa1d1bd8804577392a94333e77b169232979dfd
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-08 10:07:56 +0800

    10.8 每日一题

---
+++ 每日一题/Oct/10.8.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s)
+    {
+        if(s.size()==0)
+        {
+            return;
+        }
+        for(int i=0;i<s.size()/2;i++)
+         {
+            s[i]=s[s.size()-1-i]-s[i];
+            s[s.size()-1-i]=s[s.size()-1-i]-s[i];
+            s[i]=s[i]+s[s.size()-1-i];
+         }
+         return;
+    }
+};
