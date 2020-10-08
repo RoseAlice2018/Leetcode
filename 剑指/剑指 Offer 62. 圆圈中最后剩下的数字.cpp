23c336966aed775f8c40f5d53a86011dafd11e2e
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-08 10:14:32 +0800

    update 10. 剑指 62

---
+++ 剑指/剑指 Offer 62. 圆圈中最后剩下的数字.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace  std;
+class Solution {
+public:
+    int lastRemaining(int n, int m) {
+        vector<int> temp;
+        for(int i=0;i<n;i++)
+            temp.push_back(i);
+        vector<int>::iterator it=temp.begin();
+        while(temp.size()>1)
+        {
+            int k=m%temp.size();
+            it=temp.begin()+k-1;
+            temp.erase(it);
+        }
+        return temp[0];
+    }
+};
