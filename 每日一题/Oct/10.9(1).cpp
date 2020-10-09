14124e97c86f5ed248c06cac787622228a9579fe
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-09 16:53:08 +0800

    update 10.9 slow and fast

--- 每日一题/Oct/10.9(1).cpp
+++ 每日一题/Oct/10.9(1).cpp
@@ -10,7 +10,19 @@ using namespace std;
  
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
+    bool hasCycle(ListNode *head) 
+    {
+        if(head==NULL||head->next==NULL)
+            return false;
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while (slow!=fast)
+        {
+            if(fast==NULL||fast->next==NULL)
+                return false;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return true;        
     }
 };
