8a088158c969c526be0ab14816ce5348944d76b3
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-09 17:14:34 +0800

    update 52

---
+++ 剑指/剑指 Offer 52. 两个链表的第一个公共节点.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+//  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        if(headA==headB)
+            return headB;
+        if(headB==NULL||headA==NULL)
+            return NULL;
+        ListNode* a=headA;
+        ListNode* b=headB;
+        while(a!=b)
+        {
+            if(a)a=a->next;
+            else
+            {
+                a=headB;
+            }
+            if(b)b=b->next;
+            else 
+                b=headA;            
+        }
+        return a;
+    }
+};
