534bc91c8afb72c749509b888f8025dcd3e69300
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-09 16:22:42 +0800

    update 剑指 68

--- 剑指/剑指 Offer 68 - II. 二叉树的最近公共祖先.cpp
+++ 剑指/剑指 Offer 68 - II. 二叉树的最近公共祖先.cpp
@@ -12,6 +12,12 @@ using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if(root==NULL||root==p||root==q)
+            return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL)  return right;
+        if(right==NULL)    return left;
+        return root;
     }
 };
