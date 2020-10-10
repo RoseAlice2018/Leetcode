c37ea491228d9f7c8663654703cd45797c542146
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-10 23:44:51 +0800

    update 10.10

---
+++ 每日一题/Oct/10.10.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode *> visited;
+        while (head != nullptr) {
+            if (visited.count(head)) {
+                return head;
+            }
+            visited.insert(head);
+            head = head->next;
+        }
+        return nullptr;
+    }
+};
+
