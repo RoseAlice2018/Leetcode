63badcfde8df94eb363e84574c7f6c665d39a8b5
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-11 20:01:24 +0800

    update 10.11

---
+++ 每日一题/Oct/10.11.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return false;
+        }
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int maxNum = *max_element(nums.begin(), nums.end());
+        if (sum & 1) {
+            return false;
+        }
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
+        }
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/fen-ge-deng-he-zi-ji-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
