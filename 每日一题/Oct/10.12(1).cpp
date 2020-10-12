232411639efc89b96034814cad0f65619b4f26fc
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-12 09:59:56 +0800

    update 10.12

---
+++ 每日一题/Oct/10.12(1).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    void dfs(TreeNode* root, int& pre, int& ans) {
+        if (root == nullptr) {
+            return;
+        }
+        dfs(root->left, pre, ans);
+        if (pre == -1) {
+            pre = root->val;
+        } else {
+            ans = min(ans, root->val - pre);
+            pre = root->val;
+        }
+        dfs(root->right, pre, ans);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        int ans = INT_MAX, pre = -1;
+        dfs(root, pre, ans);
+        return ans;
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/solution/er-cha-sou-suo-shu-de-zui-xiao-jue-dui-chai-by-lee/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
