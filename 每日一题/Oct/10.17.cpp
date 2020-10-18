77f3964b2eee3e81d9dfa69de5fae82ccc9cf938
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-10-18 22:26:38 +0800

    update 10.18

---
+++ 每日一题/Oct/10.17.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        return solve(n, 0, 0, 0, 0);
+    }
+
+    int solve(int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                count += solve(n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+            }
+            return count;
+        }
+    }
+};
+
