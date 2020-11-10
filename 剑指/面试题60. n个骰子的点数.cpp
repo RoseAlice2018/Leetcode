83bbf21174805d58c01469ca6428a458f8e1624f
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-11-10 22:46:06 +0800

    update 11.10

---
+++ 剑指/面试题60. n个骰子的点数.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<double> dicesProbability(int n) 
+    {
+        vector<double> res;
+        
+        return res;
+    }
+};
