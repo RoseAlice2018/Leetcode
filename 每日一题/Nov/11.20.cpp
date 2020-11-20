210601ef5a1137a7440987ed9ea96091fbc95c5a
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-11-20 16:57:16 +0800

    update 11.20

--- 每日一题/Nov/11.20.cpp
+++ 每日一题/Nov/11.20.cpp
@@ -15,8 +15,44 @@ public:
         if(head==NULL||head->next==NULL)
             return head;
         ListNode* first=head;
-        ListNode* temp=head->next;
-        
+        ListNode* end=head;
+        ListNode* now=head->next;
+        while(now!=NULL)
+        {
+            ListNode* q=now;
+            now=now->next;
+            insert(first,end,q);
+        }
         return head;
+    }
+    void insert(ListNode* first,ListNode* end,ListNode* now)
+    {
+        ListNode* tempfirst=first;
+        ListNode* tempend=end;
+        if(now->val>end->val)
+        {
+            end->next=now;
+            end=now;
+            return;
+        }
+        if(now->val<first->val)
+        {
+            end->next=now->next;
+            now->next=first;
+            first=now;
+            return;
+        }
+        while(tempfirst!=tempend)
+        {
+            if(tempfirst->val<=now->val&&tempfirst->next->val>=now->val)
+            {
+                tempend->next=now->next;
+                now->next=tempfirst->next;
+                tempfirst->next=now;
+                //first=tempfirst;
+                return;
+            }
+            tempfirst=tempfirst->next;
+        }
     }
 };
