a222793a8adb95bb6a639d2f3901441b4a1a3c00
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-12-09 19:58:24 +0800

    update 12.9

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stack>
+#include<queue>
 using namespace std;
 int main()
 {
@@ -9,3 +11,40 @@ int main()
    cout<<10;
     return 0;
 }
+
+template <class T>
+struct TreeNode
+{
+	TreeNode* leftchild;
+	TreeNode* rightchild;
+	T data;
+	TreeNode(T x)leftchild(NULL),rightchild(NULL),data(x){}
+	~TreeNode();
+};
+
+void preorder(TreeNode<int>* root)
+{
+	if(root == NULL)
+		return;
+	stack<TreeNode<int>*> T_pre;
+	T_pre.push(root);
+    while(!T_pre.empty())
+    {
+        TreeNode<int>* temp=T_pre.top();
+        T_pre.pop();
+        cout<<temp->data;
+        if(temp->rightchild)
+            T_pre.push(temp->rightchild);
+        if(temp->leftchild)
+            T_pre.push(temp->leftchild);
+    }
+    return;
+}
+ void inorder(TreeNode<int>* root)
+ {
+     	if(root == NULL)
+		    return;
+	    stack<TreeNode<int>*> T_pre;
+	    T_pre.push(root);
+        while(!T_pre.empty())
+ }
