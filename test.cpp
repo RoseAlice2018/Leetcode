c8a569de3cbbc5607110278d5ade0ef190566096
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2020-12-10 14:11:04 +0800

    update 12.10

--- test.cpp
+++ test.cpp
@@ -46,5 +46,46 @@ void preorder(TreeNode<int>* root)
 		    return;
 	    stack<TreeNode<int>*> T_pre;
 	    T_pre.push(root);
-        while(!T_pre.empty())
+        do
+        {
+            while(root!=NULL)
+            {
+                T_pre.push(root);
+                root = root->leftchild;
+            }
+            if(!T_pre.empty())
+            {
+                TreeNode<int>* temp= T_pre.top();
+                T_pre.pop();
+                cout<<temp->data;
+                root = temp->rightchild;
+            }
+        } while (!T_pre.empty()||root!=NULL);
+        return ;
+ }
+
+ void postorder(TreeNode<int>* root)
+ {
+     if(root == NULL)
+     {
+         return ;
+     }
+     stack<TreeNode<int>*> T_post;
+     stack<TreeNode<int>*> T;
+     T_post.push(root);
+     while(!T_post.empty())
+     {
+         TreeNode<int>* curr = T_post.top();
+         T.push(curr);
+         T_post.pop();
+         if(curr->leftchild)
+            T_post.push(curr->leftchild);
+        if(curr->rightchild)
+            T_post.push(curr->rightchild);
+     }
+     while(!T.empty())
+     {
+         cout<<T.top()->data<<endl;
+         T.pop();
+     }
  }
