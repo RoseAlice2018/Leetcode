068e58ed92f0d89131106098fc80fc554081c0d9
RoseAlice2018 <40765813+RoseAlice2018@users.noreply.github.com>
2021-01-08 12:32:15 +0800

    update 1.8

---
+++ 每日一题/2021/Jan/1.8(1).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            swap(nums[start], nums[end]);
+            start += 1;
+            end -= 1;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+};
+/*
+nums = "----->-->"; k =3
+result = "-->----->";
+
+reverse "----->-->" we can get "<--<-----"
+reverse "<--" we can get "--><-----"
+reverse "<-----" we can get "-->----->"
+this visualization help me figure it out :)
+美服的解释，太漂亮了
+*/
---
+++ 每日一题/2021/Jan/1.8.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+    }
+};
